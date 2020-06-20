c6b514ba3ffe849a22f55ee2787c486f0ad2259a
Pratyay Rudra <rudrapratyay1999@gmail.com>
2020-06-20 22:11:37 +0530

    resources created

---
+++ resources/SERVER.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "HIGH": [{
+        "id": "h0",
+        "jobs": 100,
+        "process": []
+    },
+    {
+        "id": "h1",
+        "jobs": 100,
+        "process": []
+    }
+    ],
+    "MODERATE": [{
+        "id": "m0",
+        "jobs": 70,
+        "process": []
+    },
+    {
+        "id": "m1",
+        "jobs": 70,
+        "process": []
+    }
+    ],
+    "LOW": [{
+        "id": "l0",
+        "jobs": 30,
+        "process": []
+    },
+    {
+        "id": "l1",
+        "jobs": 30,
+        "process": []
+    }
+    ]
+}
